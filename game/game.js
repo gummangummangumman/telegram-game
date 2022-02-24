5ddc1944fd5c8f695e48b8bef70ad516957f7dc7
gummangummangumman <gumman@gumman.one>
2022-02-24 22:54:36 +0100

    Begynnelse på å sende high score fra frontend

--- game/game.js
+++ game/game.js
@@ -1,10 +1,60 @@
-function loopColour()
-{
-    const body = document.body;
+const API_BASE_URL = window.location.toString().substring(0, window.location.toString().indexOf("game/"));
+const curData = (location.hash || '').substring(1).replace(/[\?&].*/g, '');
+let counter = 0;
 
-    body.style.color = Math.random() > 0.5 ? "red" : "blue";
-
-    setTimeout(function() {loopColour(); }, 500);
+function plus() {
+    if(counter < 5) {
+        counter++;
+    }
+    document.getElementById("counter").innerHTML = counter;
 }
 
-window.onload = loopColour
+function minus() {
+    if(counter > 0) {
+        counter--;
+    }
+    document.getElementById("counter").innerHTML = counter;
+}
+
+
+function finish() {
+    document.getElementById("game").setAttribute("style", "display: none;");
+    document.getElementById("game_over").setAttribute("style", "");
+    document.getElementById("game_over_counter").innerHTML = counter;
+
+    send_score();
+}
+
+function play_again() {
+    counter = 0;
+    document.getElementById("counter").innerHTML = counter;
+    document.getElementById("game").setAttribute("style", "");
+    document.getElementById("game_over").setAttribute("style", "display: none;");
+}
+
+// kode under inspirert av Corsairs - https://tbot.xyz/corsairs/game.js?19
+function send_score() {
+    post(API_BASE_URL, {
+        score: counter,
+        curData: curData
+    });
+}
+
+function post(url, data) {
+    var xhr = new XMLHttpRequest();
+    var body = []
+    for(var i in data) {
+      body.push(encodeURIComponent(i) + '=' + encodeURIComponent(data[i]))
+      console.log("pushing " + encodeURIComponent(i) + '=' + encodeURIComponent(data[i]));
+    }
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status == 200) {
+        console.log(JSON.parse(xhr.responseText));
+      } else {
+        console.log("epic fail");
+      }
+    }
+    xhr.open("POST", url, true);
+    xhr.send(body.join('&'));
+  }
+  
