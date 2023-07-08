55b7141b37c972cccd9ae226f09274f547ef2d64
gummangummangumman <gumman@gumman.one>
2023-07-09 01:16:31 +0200

    Prettier on save

--- game/src/components/Counter.svelte
+++ game/src/components/Counter.svelte
@@ -30,11 +30,7 @@
 </script>
 
 <div class="counter">
-	<button
-		on:click={decrementCount}
-		disabled={count < 1}
-		aria-label="Decrease the counter by one"
-	>
+	<button on:click={decrementCount} disabled={count < 1} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
@@ -47,11 +43,7 @@
 		</div>
 	</div>
 
-	<button
-		on:click={incrementCount}
-		disabled={count > 4}
-		aria-label="Increase the counter by one"
-	>
+	<button on:click={incrementCount} disabled={count > 4} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
